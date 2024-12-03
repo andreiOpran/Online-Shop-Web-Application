de33ad72abbc2dedc4194bf93fed5bf637ee7c34
andreiOpran <andrei.opran@icloud.com>
2024-12-03 23:44:43 +0200

    Implementat model Category

---
+++ OnlineShop/Models/Category.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Category name is required. Please enter a category name.")]
+        public string? CategoryName { get; set; }
+
+        // dintr-o categorie fac parte mai multe produse
+        public virtual ICollection<Product>? Products { get; set; }
+
+        // TODO
+        // validare pe serviciu (IValidatableObject)
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (CategoryName != null && CategoryName.Length > 256)
+            {
+                yield return new ValidationResult("Category name must be at most 256 characters long.");
+            }
+            if (CategoryName != null && CategoryName.Length < 3)
+            {
+                yield return new ValidationResult("Category name must be at least 3 characters long.");
+            }
+            yield return ValidationResult.Success!;
+
+        }
+    }
+}
