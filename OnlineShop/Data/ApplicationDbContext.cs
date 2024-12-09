fd17f7362a14dbbc779132db12b154f0503ad5e8
ForceOfNature13 <cemilandrei@gmail.com>
2024-12-09 11:34:02 +0200

    Am adaugat User si Role si am configurat baza de date

--- OnlineShop/Data/ApplicationDbContext.cs
+++ OnlineShop/Data/ApplicationDbContext.cs
@@ -14,6 +14,16 @@ namespace OnlineShop.Data
         {
         }
 
+
+        //tabelele din baza de date
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CartProduct> CartProducts { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             //definirea relatiei many-to-many intre Cart si Product
--- OnlineShop/Models/ApplicationUser.cs
+++ OnlineShop/Models/ApplicationUser.cs
@@ -1,8 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace OnlineShop.Models
 {
     //Pasul 1 :Extidem functionalitatea clasei IdentityUser
     public class ApplicationUser : IdentityUser
     {
+
+        //un user poate avea mai multe produse
+        public virtual ICollection<Product>? Products { get; set; }
+
+        //un user poate avea mai multe carturi
+        public virtual ICollection<Cart>? Carts { get; set; }
+
+        //un user poate avea mai multe review-uri
+        public virtual ICollection<Review>? Reviews { get; set; }
+
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        //variabila care va fi folosita pentru a stoca rolul selectat de utilizator
+        //pentru popularea unui dropdown
+        [NotMapped]
+        public IEnumerable<SelectListItem>? AllRoles { get; set; }
     }
 }
