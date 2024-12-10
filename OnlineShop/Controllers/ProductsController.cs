862a0fdd1df3fb7b5c4adafbc23e56c5ae7d6a2b
andreiOpran <andrei.opran@icloud.com>
2024-12-10 11:23:02 +0200

    ProductsController -> Metoda Index() (contine TODO)

--- OnlineShop/Controllers/ProductsController.cs
+++ OnlineShop/Controllers/ProductsController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+using OnlineShop.Models;
 
 namespace OnlineShop.Controllers
 {
+    [Authorize]
     public class ProductsController : Controller
     {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public ProductsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        
+        // [HttpGet] implicit
+        // TODO
+        // [Authorize(Roles = "")]
         public IActionResult Index()
         {
+
+            var products = db.Products.Include("Category");
+
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.Message = TempData["message"];
+                ViewBag.Alert = TempData["messageType"];
+            }
+
+            ViewBag.Products = products;
+
+            // TODO 
+            // motor cautare
+
+            // TODO
+            // afisare paginata
+
             return View();
+                   
         }
+
     }
 }
