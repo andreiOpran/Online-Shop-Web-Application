c54ebf1524499997870eaf4e864997ce555358e1
ForceOfNature13 <cemilandrei@gmail.com>
2024-12-10 14:04:34 +0200

    Am adaugat modelul de Order

---
+++ OnlineShop/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        //foreign key
+        public int? CartId { get; set; }
+
+        public Cart? Cart { get; set; }
+
+       
+        public string? Status { get; set; }
+        public DateTime? OrderDate { get; set; }
+
+        public string? PaymentMethod { get; set; }
+
+        public string? ShippingAddress { get; set; }   
+        
+
+
+    }
+}
