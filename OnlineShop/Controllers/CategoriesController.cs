410ed76295c8ffa0f5e371a0d088df09c0cbc579
andreiOpran <andrei.opran@icloud.com>
2024-12-13 20:20:09 +0200

    Category Index View

---
+++ OnlineShop/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+
+namespace OnlineShop.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public CategoriesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            if(TempData.ContainsKey("message"))
+            {
+                ViewBag.Message = TempData["message"].ToString();
+            }
+
+            var categories = from category in db.Categories
+                             orderby category.CategoryName
+                             select category;
+            
+            ViewBag.Categories = categories;
+            return View();
+        }
+    }
+}
